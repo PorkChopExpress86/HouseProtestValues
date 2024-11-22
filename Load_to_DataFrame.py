d4ac390f4641768e3b765c25c37f0a2431d9c7ee
PorkChopExpress86 <blake.b.8726@gmail.com>
2024-11-22 09:04:03 -0600

    Fixed loading issue for assessed_per_sqft

--- Load_to_DataFrame.py
+++ Load_to_DataFrame.py
@@ -71,7 +71,7 @@ WHERE br.impr_tp = 1001
   AND br.date_erected > 1900'''
 
     df = pd.read_sql_query(sql_query, con)
-    df['assessed_per_sqft'] = df[df['assessed_val'] / df['im_sq_ft']]
+    df['assessed_per_sqft'] = df['assessed_val'] / df['im_sq_ft']
     return df
 
 
