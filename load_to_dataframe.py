1aa5af4d223d46032c458ca611bc004431f6f9a5
PorkChopExpress86 <blake.b.8726@gmail.com>
2024-11-26 08:07:18 -0600

    Removed commented out code

--- load_to_dataframe.py
+++ load_to_dataframe.py
@@ -77,45 +77,5 @@ WHERE br.impr_tp = 1001
     return df
 
 
-# def load_fixture_data():
-#     con = sqlite3.connect('HouseProtestValues.db')
-#     # Story Height Index: STY
-#     # Room: Bedroom: RMB
-#     # Room: Full Bath: RMF
-#     # Room: Half Bath: RMH
-#     # Room: Total: RMT
-#     fixtures_sql = """SELECT *
-#                       FROM "fixtures"
-#                       WHERE type IN ('STY', 'RMB','RMF','RMH','RMT')
-#                     """
-#     fixtures = pd.read_sql_query(fixtures_sql, con)
-#
-#     # Pivot table
-#     fix_pt = fixtures.pivot_table(index=['acct', 'bld_num'], columns='type', values='units', aggfunc='sum')
-#     fix_pt = fix_pt.reset_index()
-#     fix_pt.fillna(0, inplace=True)
-#
-#     return fix_pt
-#
-#
-# def merge_dataframes(base_df, fix_pt):
-#     data_df = pd.merge(base_df, fix_pt, on=['acct', 'bld_num'], how='left')
-#     data_df.dropna(inplace=True)
-#     data_df.reset_index(drop=True, inplace=True)
-#
-#     return data_df
-
-
-# def load_all():
-#     df_1 = load_building_data()
-#     df_2 = load_fixture_data()
-#     df_3 = merge_dataframes(df_1, df_2)
-#     return df_3
-
-
 if __name__ == "__main__":
-    # build_df = load_building_data()
-    # fix_df = load_fixture_data()
-    # df = merge_dataframes(build_df, fix_df)
-
     load_data_frame()
