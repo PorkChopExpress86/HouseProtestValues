17275f0589ba0642216677128e2dcac2e8852fa5
PorkChopExpress86 <blake.b.8726@gmail.com>
2024-11-26 06:14:26 -0600

    reformat and added gis information to download

--- setup.py
+++ setup.py
@@ -2,33 +2,27 @@ from download import download_link_by_url
 from unzip_and_load_db import load_tables_to_sqlite, unzip_files
 
 if __name__ == "__main__":
-
     # Download files
-    file_urls = [
-        "https://download.hcad.org/data/CAMA/2024/Real_acct_owner.zip",
+    file_urls = ["https://download.hcad.org/data/CAMA/2024/Real_acct_owner.zip",
         "https://download.hcad.org/data/CAMA/2024/Real_building_land.zip",
-        "https://download.hcad.org/data/CAMA/2024/Code_description_real.zip",
-    ]
+        "https://download.hcad.org/data/CAMA/2024/Code_description_real.zip", ]
 
-    # Download all three zips
+    # Tax data information
     ajax_site_url = "https://hcad.org/pdata/pdata-property-downloads.html"  # Replace with the target website's URL
+    download_link_by_url(ajax_site_url, file_urls, "Zips", 90)
+
+    # Download GIS data on tax particles
+    file_urls = ["https://download.hcad.org/data/GIS/Parcels.zip"]
+    ajax_site_url = "https://hcad.org/pdata/pdata-gis-downloads.html"
     download_link_by_url(ajax_site_url, file_urls, "Zips", 90)
 
     # Unzip files
     print("Extracting data...")
-    data_files = [
-        "real_neighborhood_code.txt",
-        "building_res.txt",
-        "real_acct.txt",
-        "land.txt",
-        "fixtures.txt",
-        "extra_features.txt",
-        "exterior.txt",
-        "extra_features_detail1.txt",
-    ]
+    data_files = ["real_neighborhood_code.txt", "building_res.txt", "real_acct.txt", "land.txt", "fixtures.txt",
+        "extra_features.txt", "exterior.txt", "extra_features_detail1.txt", ]
 
     # Extract files
-    unzip_files(src="Zips", dest="Data", file_list=data_files)
+    unzip_files(src="Zips", dst="Data", file_list=data_files)
 
     # Add parcels.csv
     data_files.append("parcels.csv")
@@ -37,7 +31,7 @@ if __name__ == "__main__":
     load_tables_to_sqlite(data_files)
 
     # Extract files
-    unzip_files(src="Zips", dest="Data", file_list=data_files)
+    unzip_files(src="Zips", dst="Data", file_list=data_files)
 
     # Load data into sqlite database
     load_tables_to_sqlite(data_files)
